4922ab6991880b1876e78b39e5ee57dd4c056ca2
DusanPajic002 <dusanpajic2002@gmail.com>
2024-01-06 11:17:48 +0100

    startmeni bg

--- player/Player.gd
+++ player/Player.gd
@@ -12,21 +12,32 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if is_on_floor() and ( Input.is_action_just_pressed("ui_accept") || direction_y == -1) :
 		velocity.y = JUMP_VELOCITY
 		animation.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction && velocity.y == 0:
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+	if direction:
 		velocity.x = direction * SPEED
-		animation.play("run")
-	elif velocity.y == 0:
+		if velocity.y == 0:
+			animation.play("run");
+	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
+		if velocity.y == 0:
+			if direction_y == 0:
+				animation.play("idle")
+			elif direction_y == 1:
+				animation.play("crouch")
 	if velocity.y > 0:
 		animation.play("fall")
+
 	move_and_slide()
