9da1587a8b0e58fd5a647e0ddfcb3384a9578f79
DusanPajic002 <dusanpajic2002@gmail.com>
2024-01-06 20:33:11 +0100

    1

--- enemys/frog/frog.gd
+++ enemys/frog/frog.gd
@@ -1,24 +1,30 @@
 extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var SPEED = 1
-
+var SPEED = 50
+const JUMP_VELOCITY = -300.0
+var jumped = false
 var player 
 var chase = false 
 
 func _physics_process(delta):
 	# Add the gravity
 	velocity.y += gravity * delta
-	print(chase)
 	if chase == true:
-		get_node("AnimatedSprite2D").play("jump")
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if velocity.y > 0:
+			get_node("AnimatedSprite2D").play("fall")
+			jumped = true
+		else:
+			get_node("AnimatedSprite2D").play("jump")
 		player = get_node("../../players/Player") 
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0 :	
 			get_node("AnimatedSprite2D").flip_h = true
 		else:
 			get_node("AnimatedSprite2D").flip_h = false
-		velocity.x += direction.x * SPEED
+		velocity.x = direction.x * SPEED
 	else:
 		get_node("AnimatedSprite2D").play("idle")	
 		velocity.x =0
@@ -27,9 +33,15 @@ func _physics_process(delta):
 func _player_entered(body): 
 	if body.name == "Player":
 		chase = true;
-		
 
-
-func _on_area_2d_body_exited(body):
+func _player_exited(body):
 	if body.name == "Player":
 		chase = false # Replace	 with function body.
+
+func _on_death_body_entered(body):
+	if body.name == "Player":
+		var player = get_node("../../players/Player") 
+		var direction = (player.position - self.position).normalized()
+		print(direction.y)
+		if direction.y < -0.9:
+			self.queue_free()
