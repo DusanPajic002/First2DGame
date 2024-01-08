dc10139307b95aa4879735694c229cc25e38d3f8
DusanPajic002 <dusanpajic2002@gmail.com>
2024-01-08 15:48:47 +0100

    1

--- enemys/bat/bat.gd
+++ enemys/bat/bat.gd
@@ -2,29 +2,36 @@ extends CharacterBody2D
 
 var health = 6
 var SPEED = 40
-var direction = 1
+var direction_x = randi_range(0, 1) 
+const LEFT_BORDER = 150
+const RIGHT_BORDER = 550
+
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	get_node("AnimatedSprite2D").flip_h = true
 	get_node("AnimatedSprite2D").play("fly")
-	velocity.y =0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	velocity.y  = 0
+	print(direction_x)
 	get_node("AnimatedSprite2D").play("fly")
-	if  position.x < 250:
-		get_node("AnimatedSprite2D").flip_h = true
-		direction = 1
-	elif position.x > 550:
-		get_node("AnimatedSprite2D").flip_h = false
-		direction = -1
-	velocity.x = direction * SPEED
+	side(direction_x)
+	velocity.x = direction_x * SPEED
+	#velocity.y = 0
+	
+	if  position.x < LEFT_BORDER:
+		direction_x = 1
+	elif position.x > RIGHT_BORDER:
+		direction_x = -1
+		
 	move_and_slide()
 		
 
 func _killbox_body_entered(body):
 	if body.name == "Player":
 		self.health -=3
+		print(body.velocity)
 		#if health <=0:
 			#get_node("AnimatedSprite2D").play("death")
 			#await get_node("AnimatedSprite2D").animation_finished
@@ -35,4 +42,9 @@ func _on_demagebox_body_entered(body):
 	if body.name == "Player":
 		body.health -= 4
 		
-
+func side(direction):
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = false
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = true
+	
--- player/Player.gd
+++ player/Player.gd
@@ -1,62 +1,43 @@
 extends CharacterBody2D
 
 var health = 30
-const SPEED = 200.0
+const SPEED = 180
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animation = get_node("AnimationPlayer")
 var double_jump = true  
 
 func _physics_process(delta):
-	# Add the gravity.
+	
 	velocity.y += gravity * delta
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
-	if direction_x == -1:
-		get_node("AnimatedSprite2D").flip_h = true
-	elif direction_x == 1:
-		get_node("AnimatedSprite2D").flip_h = false
-	velocity.x = direction_x * SPEED
-	print(direction_x)
+	side(direction_x)
+	
 	if is_on_floor():
 		if direction_x == 0:
 			animation.play("idle")
 		else:
-			
 			animation.play("run");
-				
-	
-	
-	if (is_on_floor() or double_jump) and Input.is_action_just_pressed("ui_accept"):
+	elif velocity.y > 150:
+		animation.play("fall")
+		
+	if (double_jump or is_on_floor()) and Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 		animation.play("jump")
 		if not is_on_floor():
-			double_jump = false  # Iskoristite dvostruki skok
+			double_jump = false
 		else:
-			double_jump = true 
-	elif velocity.y > 0 and not is_on_floor():
-		animation.play("fall")
-		
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#if direction == -1:
-		#get_node("AnimatedSprite2D").flip_h = true
-	#elif direction == 1:
-		#get_node("AnimatedSprite2D").flip_h = false
-	#if direction:
-		#velocity.x = direction * SPEED
-		#if velocity.y == 0:
-			#animation.play("run");
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#if velocity.y == 0:
-			#if direction_y == 0:
-				#animation.play("idle")
-			#elif direction_y == 1:
-				#animation.play("crouch")
-	#if velocity.y > 0:
-
+			double_jump = true
+			
 	move_and_slide()
+
+func side(direction):
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+	velocity.x = direction * SPEED
+	
